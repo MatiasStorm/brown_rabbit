a8549d57a84f211799c033e60f644ac00ab950aa
MatiasStorm <48760011+MatiasStorm@users.noreply.github.com>
2019-08-10 23:07:02 +0200

    Added header and slider

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,4 +1,13 @@
-window.onload = function(){
-    document.body.innerHTML = document.body.innerHTML + "<div> RABBIT </div>";
-};
+/*Slider*/
+
+function showSlide(index){
+  var images = document.getElementsByClassName("slide-img");
+  var buttons = document.getElementsByClassName("slider-button");
+  for(i = 0; i < images.length; i++){
+    images[i].style.display = "none";
+    buttons[i].style.backgroundColor = "rgb(44, 131, 176)";
+  }
+  images[index].style.display = "inline";
+  buttons[index].style.backgroundColor = "rgb(163, 163, 163)";
+}
 
