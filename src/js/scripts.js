721d7af05fcc0ef2926203a812c04f9af41297b6
MatiasStorm <48760011+MatiasStorm@users.noreply.github.com>
2019-08-11 13:18:04 +0200

    Website is finished

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -34,26 +34,54 @@ showRandomSlide();
 
 
 
-/* Article read more functions*/
+/* Article page functions*/
+PAGE_INDEX = 0;
+N_PAGES = 3;
+ARTICLES_PR_PAGE = 3;
 function readMore(artIndex){
-  articles = document.getElementsByClassName("article");
-  hiddenText = articles[artIndex].getElementsByClassName("hidden-text")[0];
-  button = articles[artIndex].getElementsByTagName("button")[0];
-  dots = articles[artIndex].getElementsByClassName("three-dots")[0];
+  var articles = document.getElementsByClassName("article");
+  var hiddenText = articles[artIndex].getElementsByClassName("hidden-text")[0];
+  var button = articles[artIndex].getElementsByTagName("button")[0];
+  var dots = articles[artIndex].getElementsByClassName("three-dots")[0];
   if(!hiddenText.style.display){
     hiddenText.style.display = "inline";
     dots.style.display = "none";
-    button.innerHTML = "Show less"
-    console.log(button.style.marginLeft)
+    button.style.display = "none";
   }
-  else{
-    button.innerHTML = "Read more"
-    dots.style.display = "inline"
-    hiddenText.style.display = null;
-  }
-  
 }
 
+function hideAllText(pageIndex){
+  var statIndex = pageIndex * (ARTICLES_PR_PAGE);
+  var maxIndex = pageIndex * ARTICLES_PR_PAGE + ARTICLES_PR_PAGE;
+  var articles = document.getElementsByClassName("article");
+  for(i = pageIndex * (ARTICLES_PR_PAGE); i < maxIndex; i++){
+    var hiddenText = articles[i].getElementsByClassName("hidden-text")[0];
+    var button = articles[i].getElementsByTagName("button")[0];
+    var dots = articles[i].getElementsByClassName("three-dots")[0];
+    button.style.display = "inline";
+    dots.style.display = "inline";
+    hiddenText.style.display = null;
+  }
+}
 
+function changePage(pageIndex){
+  var pages = document.getElementsByClassName("article-container");
+  var buttons = document.getElementsByClassName("page-button");
+  var span = document.getElementsByClassName("page-of")[0];
+  for(i = 0; i < pages.length; i++){
+    pages[i].style.display = "none";
+    buttons[i].classList.remove("page-button-active");
+  }
+  pages[pageIndex].style.display = "block";
+  buttons[pageIndex].classList.add("page-button-active");
+  span.innerHTML = "Page " + (pageIndex + 1) + " of " + N_PAGES;
+  hideAllText(pageIndex);
+  PAGE_INDEX = pageIndex;
+}
 
-
+function nextPage(){
+  if(PAGE_INDEX < N_PAGES - 1){
+    PAGE_INDEX += 1;
+    changePage(PAGE_INDEX);
+  }
+}
